d314ea9830155478251623137abda663a07c285a
AMER\amanda.langley <amanda.jo.langley@gmail.com>
2022-03-14 14:32:08 -0700

    committing existing project

---
+++ ConsoleApp1/ConsoleApp1/GoogleTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GoogleTest
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void OpenBrowser()
+        {
+            driver.Url = "http://the-internet.herokuapp.com/";
+        }
+
+        [Test]
+        public void FindByTextTest()
+        {
+            driver.FindElement(By.XPath("//h2[contains(text(),'Available Examples')]"));
+
+        }
+        [Test]
+        public void WebsiteStart()
+        {
+            Assert.AreEqual(driver.Title, "The Internet");
+            IWebElement link = driver.FindElement(By.LinkText("Add/Remove Elements"));
+            link.Click();
+            // add four elements
+            for (int i=0; i<4; i++)
+            {
+                driver.FindElement(By.CssSelector(".example button[onclick= 'addElement()']")).Click();
+            }
+
+            // Find all elements and click them
+            IList<IWebElement> elements = driver.FindElements(By.CssSelector("#elements .added-manually"));
+            foreach (var element in elements)
+            {
+                element.Click();
+            }
+        }
+
+        [Test]
+        public void BasicAuthTest()
+        {
+            driver.Url = "http://admin:admin@the-internet.herokuapp.com/basic_auth";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
+            WebDriverWait wait = new WebDriverWait(driver,TimeSpan.FromSeconds(2));
+            IWebElement element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.ClassName("example")));
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Tests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Tests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Startbrowser()
+        {
+            driver = new ChromeDriver();
+           
+        }
+
+        [Test]
+        public void StartTest()
+        {
+            driver.Url = "https://www.rexelusa.com/";
+            string selector = " a[alt='Wire, Cable, & Cord'] div[class='v-card__text']";
+
+            //IWebElement element = driver.FindElement(By.CssSelector("a[href = /s/electrical-power-distribution?cat=yrikh6]"));
+            IWebElement element2 = driver.FindElement(By.CssSelector(selector));
+            element2.Click();
+            driver.Manage().Window.Size = new Size(1000, 650);
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/main/div/div/div/div[2]/div[2]/div[2]/div/div/div/div/div/div")).Click();
+
+            
+
+            //driver.FindElement(By.CssSelector("div#v-card__text div[text=Wire, Cable, & Cord]"));
+            //element.SendKeys("test@gmail.com");
+            //driver.FindElement(By.CssSelector("#philadelphia-field-submit")).Submit();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            //driver.Quit();
+        }
+    }
+}
